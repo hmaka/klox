668bf875cd2a624d06c216493471cea36755fc58
Havish <Havish747@gmail.com>
2025-11-20 16:17:15 -0800

    Basic usage and user input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,16 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+fun main(args: Array<String>) {
+    println("Arguments: ${args.joinToString()}")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    when (args.size) {
+        0 -> println("starting interactive shell")
+        1 -> println("starting program from file")
+        else -> error("Usage: <file-name> | ")
     }
+//    while (true){
+//        val input = readln()
+//        println("ouput: " + input)
+//    }
+
 }
