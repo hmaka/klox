b6d8e0610488e9e3e5b6561a2a9fc0bc90fe2c2b
Havish <Havish747@gmail.com>
2025-11-23 13:10:14 -0800

    Changes to Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,49 @@
 package org.root
 
-fun main(args: Array<String>) {
-    println("Arguments: ${args.joinToString()}")
+import Scanner
+import java.io.File
+import java.io.IO.readln
+import kotlin.system.exitProcess
 
-    when (args.size) {
-        0 -> println("starting interactive shell")
-        1 -> println("starting program from file")
-        else -> error("Usage: <file-name> | ")
+class Lox {
+    private var hadError = false
+
+    fun main(args: Array<String>) {
+        when (args.size) {
+            0 -> promptLoop()
+            1 -> runFile(args.first())
+            else -> error("Usage: <file-name> | ")
+        }
+        if (hadError) exitProcess(65)
     }
-//    while (true){
-//        val input = readln()
-//        println("ouput: " + input)
-//    }
+
+    private fun runFile(path: String) {
+        val resource = Lox::class.java.getResource(path)
+        val input = resource?.let { File(it.toURI()).readText() } ?: error("Input File Not Found")
+        run(input)
+    }
+
+    fun promptLoop() {
+        println("starting interactive shell")
+        while (true) {
+            val input = readln("> ")
+            run(input)
+            hadError = false
+        }
+    }
+
+    private fun run(source: String) {
+        val scanner = Scanner(source, this)
+        val tokens = scanner.scanTokens().forEach { println(it) }
+    }
+
+    fun error(line: Int, message: String) {
+        report(line, "", message)
+        hadError = true
+    }
+
+    private fun report(line: Int, where: String, message: String) {
+        System.err.println("[line $line] Error $where : $message")
+    }
 
 }
